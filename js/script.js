fb2951c4be310a5475f7fb1b1049c2c486583747
guilhermeeHenrique <102684487+guilhermeeHenrique@users.noreply.github.com>
2026-03-13 11:20:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("VerifyPolitics loaded");
+
+document.querySelectorAll('.faq-item h3').forEach((title) => {
+  title.style.cursor = 'pointer';
+  const content = title.nextElementSibling;
+  content.style.display = 'none';
+
+  title.addEventListener('click', () => {
+    content.style.display = content.style.display === 'none' ? 'block' : 'none';
+  });
+});
